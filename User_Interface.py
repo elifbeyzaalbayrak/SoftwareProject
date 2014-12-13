c058837b1a652c022694cbe00ee5fdd74e935237
elifbeyzaalbayrak <elifalbayrak@std.sehir.edu.tr>
2014-12-13 20:50:22 +0200

    commit3
    gender eksiği giderildi

--- User_Interface.py
+++ User_Interface.py
@@ -4,6 +4,9 @@ import tkMessageBox
 import sqlite3
 import time
 
+userlst=[]
+
+
 
 def logInWindow():
     root.destroy()
@@ -16,12 +19,31 @@ def logInWindow():
     username_entry=Entry(root2,width=30)
     password_entry=Entry(root2,width=30)
 
-    login_button=Button(root2,text="Log In",width=30)
-
     label_username.grid(row=1,column=1)
     label_password.grid(row=2,column=1)
     username_entry.grid(row=1,column=2)
     password_entry.grid(row=2,column=2)
+
+
+
+    def check_user_info():
+        username=username_entry.get()
+        password=password_entry.get()
+        db=sqlite3.connect("user_data.db")
+        im=db.cursor()
+        im.execute("""SELECT * FROM user_info where username = ? AND password = ?""",(username,password))
+        data=im.fetchone()
+        if data==None:
+            tkMessageBox.showerror("Error!","User Name or Password is wrong!")
+        else:
+            tkMessageBox.showinfo("Welcome!","Welcome Back!")
+            userlst.append(username)
+            userlst.append(password)
+            root2.destroy()
+            welcome_window()
+
+
+    login_button=Button(root2,text="Log In",width=30,command=check_user_info)
     login_button.grid(row=3,column=1,columnspan=2)
 
 
@@ -38,6 +60,7 @@ def newUserWindow():
     weight_label=Label(root3,text="Weight: ")
     age_label=Label(root3,text="Age: ")
     goal_label=Label(root3,text="Select Goal: ")
+    gender_label=Label(root3,text="Gender: ")
 
     username_entry=Entry(root3,width=30)
     password_entry=Entry(root3,width=30)
@@ -46,6 +69,7 @@ def newUserWindow():
     age_entry=Entry(root3,width=30)
 
     radiovar=IntVar()
+    radiovar2=IntVar()
 
     rb1=Radiobutton(root3,text="Lose 1 kg per week",variable=radiovar,value=1)
     rb2=Radiobutton(root3,text="Lose 0.8 kg per week",variable=radiovar,value=2)
@@ -57,14 +81,16 @@ def newUserWindow():
     rb8=Radiobutton(root3,text="Gain 0.8 kg per week",variable=radiovar,value=8)
     rb9=Radiobutton(root3,text="Gain 1 kg per week",variable=radiovar,value=9)
 
-
+    rb10=Radiobutton(root3,text="Male",variable=radiovar2,value=1)
+    rb11=Radiobutton(root3,text="Female",variable=radiovar2,value=2)
 
     select_username_lbl.grid(row=1,column=1)
     select_password_lbl.grid(row=2,column=1)
     height_label.grid(row=3,column=1)
     weight_label.grid(row=4,column=1)
     age_label.grid(row=5,column=1)
-    goal_label.grid(row=6,column=1)
+    gender_label.grid(row=6,column=1)
+    goal_label.grid(row=8,column=1)
 
     username_entry.grid(row=1,column=2)
     password_entry.grid(row=2,column=2)
@@ -72,15 +98,18 @@ def newUserWindow():
     weight_entry.grid(row=4,column=2)
     age_entry.grid(row=5,column=2)
 
-    rb1.grid(row=6,column=2)
-    rb2.grid(row=7,column=2)
-    rb3.grid(row=8,column=2)
-    rb4.grid(row=9,column=2)
-    rb5.grid(row=10,column=2)
-    rb6.grid(row=11,column=2)
-    rb7.grid(row=12,column=2)
-    rb8.grid(row=13,column=2)
-    rb9.grid(row=14,column=2)
+    rb10.grid(row=6,column=2)
+    rb11.grid(row=7,column=2)
+
+    rb1.grid(row=8,column=2)
+    rb2.grid(row=9,column=2)
+    rb3.grid(row=10,column=2)
+    rb4.grid(row=11,column=2)
+    rb5.grid(row=12,column=2)
+    rb6.grid(row=13,column=2)
+    rb7.grid(row=14,column=2)
+    rb8.grid(row=15,column=2)
+    rb9.grid(row=16,column=2)
 
 
     def add_to_database():
@@ -97,8 +126,11 @@ def newUserWindow():
         age=int(age_entry.get())
         goal=radiovar.get()
         weight2=weight
-        data=(user,password,age,weight,height,goal,weight2)
-        im.execute("""INSERT INTO user_info VALUES (?,?,?,?,?,?,?)""",data)
+        gender=radiovar2.get()
+        userlst.append(user)
+        userlst.append(password)
+        data=(user,password,age,weight,height,gender,goal,weight2)
+        im.execute("""INSERT INTO user_info VALUES (?,?,?,?,?,?,?,?)""",data)
         im.close()
         db.commit()
         db.close()
@@ -108,10 +140,9 @@ def newUserWindow():
 
 
     sign_up_button=Button(root3,text="Sign Up",width=30,command=add_to_database)
-    sign_up_button.grid(row=15,column=1,columnspan=2)
+    sign_up_button.grid(row=17,column=1,columnspan=2)
 
     root3.mainloop()
-
 
 def welcome_window():
     root4=Tk()
@@ -119,6 +150,7 @@ def welcome_window():
 
 root=Tk()
 root.title("LogIn")
+
 content=Frame(root)
 login=Button(content,text="Log In",command=logInWindow,width=15)
 newuser=Button(content,text="New User",command=newUserWindow,width=15)
@@ -131,5 +163,5 @@ login.grid(row=2,column=1)
 newuser.grid(row=2,column=2)
 orlbl.grid(row=3,column=1,columnspan=2)
 exit_button.grid(row=4,column=1,columnspan=2)
-
 root.mainloop()
+
--- create_user_database.py
+++ create_user_database.py
@@ -2,7 +2,7 @@ __author__ = 'elifalbayrak'
 import sqlite3
 db=sqlite3.connect("user_data.db")
 im=db.cursor()
-#im.execute("""CREATE TABLE user_info (username,password,age,weight1,height,goal,weight2)""")
+#im.execute("""CREATE TABLE user_info (username,password,age,weight1,height,gender,goal,weight2)""")
 
 
 im.execute("""SELECT * FROM user_info""")
